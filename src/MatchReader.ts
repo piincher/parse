faf39f15b81d88767fbd46cb5a3b32d585525516
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-04 07:54:36 +0800

    refactor again using interface

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,19 +1,23 @@
-import { CsvFileReader } from './CsvFileReader';
 import { dateStringToDate } from './utils';
 import { MatchResults } from './MatchResult';
-
 type MatchData = [Date, string, string, number, number, MatchResults, string];
+interface DataReader {
+	read(): void;
+	data: string[][];
+}
 
-export class MatchReader extends CsvFileReader<MatchData> {
-	mapRow(row: string[]): MatchData {
-		return [
-			dateStringToDate(row[0]),
-			row[1],
-			row[2],
-			parseInt(row[3]),
-			parseInt(row[4]),
-			row[5] as MatchResults,
-			row[6]
-		];
-	}
+export class MatchReader {
+	constructor(public reader: DataReader) {}
+
+	// .map((row: string[]): MatchData => {
+	// 			return [
+	// 				dateStringToDate(row[0]),
+	// 				row[1],
+	// 				row[2],
+	// 				parseInt(row[3]),
+	// 				parseInt(row[4]),
+	// 				row[5] as MatchResults,
+	// 				row[6]
+	// 			];
+	// 		});
 }

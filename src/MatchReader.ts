ac3194486e6196639cec9ee610e70e833fb9d2e7
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-04 08:04:03 +0800

    transforming data

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -7,17 +7,21 @@ interface DataReader {
 }
 
 export class MatchReader {
+	matches: MatchData[] = [];
 	constructor(public reader: DataReader) {}
 
-	// .map((row: string[]): MatchData => {
-	// 			return [
-	// 				dateStringToDate(row[0]),
-	// 				row[1],
-	// 				row[2],
-	// 				parseInt(row[3]),
-	// 				parseInt(row[4]),
-	// 				row[5] as MatchResults,
-	// 				row[6]
-	// 			];
-	// 		});
+	load(): void {
+		this.reader.read();
+		this.matches = this.reader.data.map((row: string[]): MatchData => {
+			return [
+				dateStringToDate(row[0]),
+				row[1],
+				row[2],
+				parseInt(row[3]),
+				parseInt(row[4]),
+				row[5] as MatchResults,
+				row[6]
+			];
+		});
+	}
 }
